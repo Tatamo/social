bbd2d384347bb5f96454af79c03a766e2ec47146
Tatamo <nsorder@gmail.com>
2023-07-07 06:46:49 +0900

    implement /nodeinfo/2.1

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import {FastifyPluginAsync} from "fastify"
 import fp from 'fastify-plugin'
 import wellknown from "./well-known";
 import {AppOptions} from "../options";
+import nodeinfo from "./nodeinfo";
 
 const routes: FastifyPluginAsync<AppOptions> = async (fastify, opts): Promise<void> => {
     fastify.register(wellknown, opts);
+    fastify.register(nodeinfo, opts);
 }
 
 export default fp(routes);
---
+++ src/routes/nodeinfo.ts
@@ -0,0 +1,31 @@
+import {FastifyPluginAsync} from "fastify"
+import fp from 'fastify-plugin'
+
+const nodeinfo: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+    fastify.get('/nodeinfo/2.1', async function (request, reply) {
+        reply.type(`application/json; profile="http://nodeinfo.diaspora.software/ns/schema/2.1#"`)
+        return {
+            "version": "2.1",
+            "software": {
+                "name": "tatamo-social",
+                "version": "0.0.1",
+                "repository": "https://github.com/Tatamo/social"
+            },
+            "protocols": [
+                "activitypub"
+            ],
+            "services": {
+                "inbound": [],
+                "outbound": []
+            },
+            "openRegistrations": false,
+            "usage": {
+                // TODO: output total users, total posts, etc.
+                "users": {}
+            },
+            "metadata": {}
+        }
+    })
+}
+
+export default fp(nodeinfo);
