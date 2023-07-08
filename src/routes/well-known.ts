19fb911326287dfb393d385ccebb1145c82d500c
Tatamo <nsorder@gmail.com>
2023-07-09 04:39:21 +0900

    avoid to show unsupported version in /.well-known/nodeinfo

--- src/routes/well-known.ts
+++ src/routes/well-known.ts
@@ -21,13 +21,17 @@ const wellknown: FastifyPluginAsync<{
 }> = async (fastify, opts): Promise<void> => {
     fastify.get('/.well-known/nodeinfo', async function (request, reply) {
         return {
-            "links": [{
-                "rel": "http://nodeinfo.diaspora.software/ns/schema/2.0",
-                "href": `${opts.protocol}${opts.host}/nodeinfo/2.0`
-            }, {
-                "rel": "http://nodeinfo.diaspora.software/ns/schema/2.1",
-                "href": `${opts.protocol}${opts.host}/nodeinfo/2.1`
-            }]
+            "links": [
+                /*
+                {
+                    "rel": "http://nodeinfo.diaspora.software/ns/schema/2.0",
+                    "href": `${opts.protocol}${opts.host}/nodeinfo/2.0`
+                },
+                */
+                {
+                    "rel": "http://nodeinfo.diaspora.software/ns/schema/2.1",
+                    "href": `${opts.protocol}${opts.host}/nodeinfo/2.1`
+                }]
         };
     });
 
