2f7493cf5e8206e0c560ef42265c73cccf5f5c11
Tatamo <nsorder@gmail.com>
2023-07-14 07:54:15 +0900

    add user and image domain objects

---
+++ src/domain/activitystream.ts
@@ -0,0 +1,96 @@
+import {User} from "./user";
+import {Image} from "./image";
+
+export type NestedActivityStreamObject<T> = Omit<T, "@context">;
+
+export type ActivityStreamContext = [string, string, { [key: string]: string }?]; // TODO: add more detailed type
+
+export type ActivityStreamPerson = {
+    "@context": ActivityStreamContext;
+    type: "Person";
+    id: `${string}/users/${string}`;
+    preferredUsername: string;
+    name: string;
+    url: string;
+    summary: string;
+    icon: string | Array<string> | NestedActivityStreamObject<ActivityStreamImage>;
+    following: `${string}/users/${string}/following`;
+    followers: `${string}/users/${string}/followers`;
+    featured: `${string}/users/${string}/featured`;
+    inbox: `${string}/users/${string}/inbox`;
+    outbox: `${string}/users/${string}/outbox`;
+    sharedInbox: `${string}/inbox`;
+    discoverable: boolean;
+    publicKey: {
+        id: `${string}/users/${string}#main-key`;
+        type: "Key";
+        owner: `${string}/users/${string}`;
+        publicKeyPem: string;
+    }
+    attachment: Array<{ type: "PropertyValue"; name: string; value: string }>;
+};
+
+export function ActivityStreamPersonFromDomainObject(user: User, publicKeyPem: string, icon: Image, opts: {
+    protocol: string,
+    host: string
+}): ActivityStreamPerson {
+    return {
+        "@context": [
+            "https://www.w3.org/ns/activitystreams",
+            "https://w3id.org/security/v1",
+            {
+                "featured": "toot:featured",
+                "discoverable": "toot:discoverable",
+            }
+        ],
+        type: "Person",
+        id: `${opts.protocol}${opts.host}/users/${user.name}`,
+        preferredUsername: user.name,
+        name: user.displayName,
+        url: `${opts.protocol}${opts.host}/@${user.name}`,
+        summary: user.summary,
+        icon: {
+            type: "Image",
+            url: "https://gts.udon.zip/fileserver/01QN6PDCZN794E99NZTCG2K4JN/attachment/original/01VQGTQEH7RQA3C10VYWSSV677.png",
+            sensitive: false
+        },
+        following: `${opts.protocol}${opts.host}/users/${user.name}/following`,
+        followers: `${opts.protocol}${opts.host}/users/${user.name}/followers`,
+        featured: `${opts.protocol}${opts.host}/users/${user.name}/featured`,
+        inbox: `${opts.protocol}${opts.host}/users/${user.name}/inbox`,
+        outbox: `${opts.protocol}${opts.host}/users/${user.name}/outbox`,
+        sharedInbox: `${opts.protocol}${opts.host}/inbox`,
+        discoverable: true,
+        publicKey: {
+            id: `${opts.protocol}${opts.host}/users/${user.name}#main-key`,
+            type: "Key",
+            owner: `${opts.protocol}${opts.host}/users/${user.name}`,
+            publicKeyPem: publicKeyPem
+        },
+        attachment: user.properties.map(({name, value}) => ({type: "PropertyValue", name, value}))
+    };
+}
+
+export type ActivityStreamImage = {
+    "@context": ActivityStreamContext;
+    type: "Image";
+    name?: string;
+    url: string;
+    sensitive?: boolean;
+};
+
+export function ActivityStreamImageFromDomainObject(image: Image, opts: {
+    protocol: string,
+    host: string
+}): ActivityStreamImage {
+    return {
+        "@context": [
+            "https://www.w3.org/ns/activitystreams",
+            "https://w3id.org/security/v1"
+        ],
+        type: "Image",
+        name: image.name,
+        url: image.url,
+        sensitive: image.sensitive
+    };
+}
---
+++ src/domain/user.ts
@@ -0,0 +1,18 @@
+import {exportPublicKeyPem} from "./sign";
+
+export type User = {
+    /**
+     * unique, not changeable string and same as the userpart of the `acct` URL
+     * this is NOT same as `user` member in Actor, but same as `preferredUsername`
+     */
+    name: string;
+
+    /**
+     * same as `name` member in Actor
+     */
+    displayName: string;
+
+    summary: string;
+
+    properties: Array<{ name: string, value: string }>;
+}
