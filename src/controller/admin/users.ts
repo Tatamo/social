f3a72ac4df1aeb3dcbfc45d6010fb4ac85dbee4a
Tatamo <nsorder@gmail.com>
2023-07-21 00:16:18 +0900

    implement addUser endpoint

--- src/controller/admin/users.ts
+++ src/controller/admin/users.ts
@@ -1,7 +1,17 @@
 import {Controller} from "../helper";
+import {User} from "../../domain/user";
 
 export const createUser: Controller<{
     Body: { name: string, displayName: string, language?: string, summary?: string }
 }> = repositories => async (request, reply) => {
     const {userRepository} = repositories;
+    const {name, displayName, language, summary} = request.body;
+    const user: User = {
+        name,
+        displayName,
+        language: language ?? "ja",
+        summary: summary ?? "",
+        properties: []
+    }
+    userRepository.save(user);
 }
